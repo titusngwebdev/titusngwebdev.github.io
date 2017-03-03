6f73b12ff96a58d2a2fa46289b553e2230c9c6da
titusngwebdev <titusngtestingweb@gmail.com>
2017-03-03 23:58:42 +0800

    Update BouncyFace.js

--- BouncyFace/BouncyFace.js
+++ BouncyFace/BouncyFace.js
@@ -1,20 +1,11 @@
 var app = new PIXI.Application(800, 600, {backgroundColor : 0x25F8FF});
 document.body.appendChild(app.view);
 var face = PIXI.Sprite.fromImage('assets/face.png')
+var xvel=Math.random()*6-3;
+var yvel=Math.random()*6-3;
 face.anchor.set(0.5);
 face.x = app.renderer.width / 2;
 face.y = app.renderer.height / 2;
-app.stage.addChild(face);
-var xvel=Math.random()*6-3;
-var yvel=Math.random()*6-3;
-app.ticker.add(function(delta) {
-    face.x += xvel * delta;
-    face.y += yvel * delta;
-    if(face.x<face.width/2||face.x>app.renderer.width-face.width/2)
-        xvel*=-1;
-    if(face.y<face.height/2||face.y>app.renderer.height-face.height/2)
-        yvel*=-1;
-});
 face.interactive=true;
 face.stage.on('pointerdown', onClick);
 function onClick()
@@ -24,3 +15,12 @@ function onClick()
     face.x = app.renderer.width / 2;
     face.y = app.renderer.height / 2;
 }
+app.stage.addChild(face);
+app.ticker.add(function(delta) {
+    face.x += xvel * delta;
+    face.y += yvel * delta;
+    if(face.x<face.width/2||face.x>app.renderer.width-face.width/2)
+        xvel*=-1;
+    if(face.y<face.height/2||face.y>app.renderer.height-face.height/2)
+        yvel*=-1;
+});
