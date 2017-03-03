00704f4570152aa89a4cd52109f9e5d8daadbeca
titusngwebdev <titusngtestingweb@gmail.com>
2017-03-03 15:23:20 +0800

    Delete BouncyFace.js

--- BouncyBall/BouncyFace.js
+++
@@ -1,7 +0,0 @@
-var app = new PIXI.Application(800, 600, {backgroundColor : 0xCCEEFF});
-document.body.appendChild(app.view);
-var face=PIXI.Sprite.fromImage('assets/face.png');
-face.anchor.set(0.5);
-face.x = app.renderer.width / 2;
-face.y = app.renderer.height / 2;
-app.stage.addChild(fce);
