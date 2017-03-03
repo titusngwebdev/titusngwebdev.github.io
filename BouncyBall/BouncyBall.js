7acb7406ee19161a6318d004fd77fe67904de78d
titusngwebdev <titusngtestingweb@gmail.com>
2017-03-03 13:45:29 +0800

    Update BouncyBall.js

--- BouncyBall/BouncyBall.js
+++ BouncyBall/BouncyBall.js
@@ -1,6 +1,6 @@
 var app = new PIXI.Application(800, 600, {backgroundColor : 0xCCEEFF});
 document.body.appendChild(app.view);
-var ball=PIXI.Sprite.fromImage('https://titusngwebdev.github.io/BouncyBall/assets/ball.png');
+var ball=PIXI.Sprite.fromImage('assets/ball.png');
 ball.anchor.set(0.5);
 ball.x = app.renderer.width / 2;
 ball.y = app.renderer.height / 2;
